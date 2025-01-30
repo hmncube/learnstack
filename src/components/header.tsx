a9cb824e1dfc9371da1f739cf322daf61d273107
happinessncube <happinessmncube@gmail.com>
2025-01-30 08:18:12 +0200

    Add basic header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import { css } from "../../styled-system/css";
+import { HStack } from "../../styled-system/jsx";
+import { Camera, CircleUserRound } from 'lucide-react';
+
 function Header() {
-    return <h1>Header</h1>
+    return <div className={css({ borderBottom: '1px solid black', padding: 4, shadow: 'md', })}>
+        <HStack justifyContent={'space-between'}>
+            <Link className={css({ m: 4, fontSize: '2xl'})} href="/Home">LearnStack</Link>
+            <Link href=''><CircleUserRound className={css({ color: 'gray', })} size={48} /></Link>
+        </HStack>
+    </div>
 }
 
 export default Header;
